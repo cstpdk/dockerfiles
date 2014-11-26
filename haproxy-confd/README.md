59f2257f241f927bc17f0077bacfb3338eef1c0b
Christoffer Pedersen <mail@cstp.dk>
2014-11-26 10:10:04 +0000

    merged ssl with master. Made a nicer testsuite, and improved robustness in general

--- haproxy-confd/README.md
+++ haproxy-confd/README.md
@@ -6,26 +6,33 @@ update a haproxy instance accordingly.
 Entries have the form:
 
 - /services/entryname
-- /services/entryname/scheme <- Possible values http,https,tcp
-- /services/entryname/host_port <- Optional, listen on this port for
-- incoming requests to this entry
-- /services/entryname/hosts/[1,2,3,4] <- Value is endpoints of IP:Port
+- /services/entryname/scheme
+	- Possible values http,https,tcp
+- /services/entryname/host_port
+	- Optional, listen on this port for incoming requests to this entry
+- /services/entryname/hosts/[1,2,3,4]
+	- Value is endpoints of IP:Port
 
-NOTE: For https, the ssl is terminated when hitting haproxy, and
-continues forward as a plain http request. SSL .pem files should be
+If any of /services/\*/scheme has value "https", then ssl is assumed.
+This works in the following way:
+
+The ssl is terminated when hitting haproxy, and
+continues forward as a plain http request. SSL .pem files must be
 mounted in /keys folder. Haproxy will use this folder as crt. This
-means that there must only be .pem files in the folder
+means that there must only be .pem files in the folder, and that there
+MUST be at least one.
 
 Entries are then available by requesting the service with either
-url path matching entryname or host header matching entryname, that is
-(assuming host is localhost):
+url path matching entryname or host header matching entryname.
 
-localhost/entryname
-entryname.localhost (assuming correct host headers)
+Example where host is localhost:
+
+http://localhost/entryname
+http://entryname.localhost
 
 If the host_port entry is given a port value then
 
-localhost:port
+http://localhost:port
 
 will also forward to the correct backend
 
--- haproxy-confd/test.sh
+++ haproxy-confd/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 #docker build -t discoverer .
 docker rm -f etcd || true &>/dev/null
@@ -13,6 +13,27 @@ sleep 3 # Yeah
 etcdctl() {
 	docker run --net=host --entrypoint /etcdctl quay.io/coreos/etcd:v0.5.0_alpha.0 "$@"
 }
+
+check(){
+	if [[ "$1" != "$2" ]] ; then
+		echo -e "Wrong test output\n"
+		echo -e "Was:\n$1"
+		echo -e "Wanted:\n$2"
+		exit 1
+	else
+		echo -e "\nOk\n"
+	fi
+}
+
+docker run -d \
+	--net=host --name discoverer \
+	-v `pwd`/haproxy.cfg:/etc/confd/templates/haproxy.cfg \
+	-v `pwd`/keys:/keys \
+	--entrypoint bash \
+	haproxy-confd \
+	-c 'confd -interval 1 -verbose -debug'
+
+sleep 3 # yeah
 
 # Happy path
 etcdctl set services/srv1/scheme http
@@ -28,31 +49,6 @@ etcdctl mkdir services/srv3
 # Missing scheme
 etcdctl set services/srv4/hosts/1 curlmyip.com:80
 
-docker run -d \
-	-p 3000:3000 -p 80:80 -p 443:443 \
-	--net=host --name discoverer \
-	-v `pwd`/haproxy.cfg:/etc/confd/templates/haproxy.cfg \
-	-v `pwd`/keys:/keys \
-	--entrypoint bash \
-	haproxy-confd \
-	-c 'confd -interval 1 -verbose -debug'
-
-sleep 3 # yeah
-
-docker cp discoverer:/etc/haproxy/haproxy.cfg from_container
-cat from_container/haproxy.cfg
-
-check(){
-	if [[ "$1" != "$2" ]] ; then
-		echo -e "Wrong test output\n"
-		echo -e "Was:\n$1"
-		echo -e "Wanted:\n$2"
-		exit 1
-	else
-		echo -e "\nOk\n"
-	fi
-}
-
 expected=$(curl -s "curlmyip.com:80")
 error_503="$(curl -s 'localhost/bogusogusogus')"
 
@@ -62,5 +58,26 @@ check "$actual" "$expected"
 actual=$(curl -s localhost/srv1)
 check "$actual" "$expected"
 
+actual=$(curl -s --resolve 'srv1.local:80:127.0.0.1' http://srv1.local)
+check "$actual" "$expected"
+
 actual=$(curl -s localhost/srv2)
 check "$actual" "$error_503"
+
+# SSL
+etcdctl set /services/config/ssl_support true
+
+etcdctl set services/srv3/scheme https
+etcdctl set services/srv3/hosts/3 curlmyip.com:80
+
+sleep 3
+
+# Happy path
+actual=$(curl --resolve 'srv3.local:443:127.0.0.1' --insecure -s https://srv3.local)
+check "$actual" "$expected"
+
+# Redirect non-https to https ... Btw curl is magic <3
+actual=$(curl --resolve 'srv3.local:80:127.0.0.1' --insecure \
+        -L -s -w "%{http_code} %{url_effective}\\n" http://srv3.local)
+
+check "$actual" "301 https://srv3.local/"
