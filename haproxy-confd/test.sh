fa9f218436d4041c7f44d301b806b990365dcf81
Christoffer Pedersen <mail@cstp.dk>
2015-06-25 15:57:12 +0000

    haproxy-confd http and https

--- haproxy-confd/test.sh
+++ haproxy-confd/test.sh
@@ -5,6 +5,7 @@
 #docker build -t discoverer .
 docker rm -f etcd || true &>/dev/null
 docker rm -f discoverer || true &>/dev/null
+docker rm -f echoserver || true &>/dev/null
 
 docker run -d -p 4001:4001 --name etcd quay.io/coreos/etcd:v0.4.6
 
@@ -26,6 +27,8 @@ check(){
 	fi
 }
 
+docker run -d --name echoserver -p 1234:1234 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf nginx
+
 docker run -d \
 	--net=host --name discoverer \
 	-v `pwd`/haproxy.cfg:/etc/confd/templates/haproxy.cfg \
@@ -42,7 +45,7 @@ etcdctl mkdir /config
 # Happy path
 etcdctl set services/srv1/scheme http
 etcdctl set services/srv1/host_port 3000
-etcdctl set services/srv1/hosts/1 curlmyip.com:80
+etcdctl set services/srv1/hosts/1 localhost:1234
 
 # Missing hosts
 etcdctl set services/srv2/scheme http
@@ -51,11 +54,11 @@ etcdctl set services/srv2/scheme http
 etcdctl mkdir services/srv3
 
 # Missing scheme
-etcdctl set services/srv4/hosts/1 curlmyip.com:80
+etcdctl set services/srv4/hosts/1 localhost:1234
 
 sleep 2
 
-expected=$(curl -s "curlmyip.com:80")
+expected=$(curl -s "localhost:1234")
 error_503="$(curl -s 'localhost/bogusogusogus')"
 
 todo="curl -s localhost:3000"
@@ -64,7 +67,7 @@ check "$actual" "$expected" "$todo"
 
 todo="curl -s localhost/srv1"
 actual=$($todo)
-check "$actual" "$expected" "$todo"
+check "$actual" "$expected" "$(curl localhost:1234/srv1)"
 
 check "$(curl -s --resolve 'srv1.local:80:127.0.0.1' http://srv1.local)" "$expected"
 
@@ -76,7 +79,7 @@ check "$actual" "$error_503" "$todo"
 etcdctl set /config/services/ssl_support true
 
 etcdctl set /services/srv5/scheme https
-etcdctl set /services/srv5/hosts/1 curlmyip.com:80
+etcdctl set /services/srv5/hosts/1 localhost:1234
 
 sleep 2
 
@@ -89,9 +92,8 @@ check "$(curl --resolve 'srv5.local:80:127.0.0.1' -s -L -w "%{http_code} %{url_e
 # Private service
 etcdctl set /services/srv6/scheme http
 etcdctl set /services/srv6/private true
-etcdctl set /services/srv6/hosts/1 curlmyip.com:80
+etcdctl set /services/srv6/hosts/1 localhost:1234
 etcdctl set /services/srv6/host_port 1338
-
 
 sleep 2
 
@@ -100,3 +102,15 @@ actual=$($todo)
 check "$actual" "$error_503" "$todo"
 
 check "$(curl -s localhost:1338)" "$expected"
+
+# Hybrid http/https
+
+etcdctl set services/srv7/scheme http-and-https
+etcdctl set services/srv7/hosts/1 localhost:1234
+
+sleep 2
+
+check "$(curl --resolve 'srv7.local:80:127.0.0.1' http://srv7.local)" \
+	"$expected" "srv7.local:80"
+check "$(curl --resolve 'srv7.local:443:127.0.0.1' --insecure \
+	-s https://srv7.local)" "$expected" "srv7.local:443"
